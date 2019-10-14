b76f898959b1a55a042357164a040c30dc004564
Emily <bruner.emilym@gmail.com>
2019-10-14 17:40:27 -0400

    completed assignment

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -51,8 +51,6 @@ console.log(subtract(1, 2))
 
 exampleArray = [1, 2, 3, 4];
 
-const triple = exampleArray.map = (num) => {
-    return num * 3;
-}
+const triple = exampleArray.map(num => num * 3);
 
-console.log(triple);
+console.log(triple)
